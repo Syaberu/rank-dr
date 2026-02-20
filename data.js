4c24348d8f17200afb07b1d1ff3a5e9685be4caa
Syaberu <syaberu9@gmail.com>
2026-02-20 21:53:00 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,8 @@
 const players = [
   { name: "MCsyaberu", tier: "HT4", rating: 0 },
-  { name: "AntiUnlockJP", tier: "HT6", rating: 0 }
+  { name: "AntiUnlockJP", tier: "HT6", rating: 0 },
+  { name: "Super Hiko14", tier: "LT7", rating: 0 },
+  { name: "KenNova758", tier: "HT8", rating: 0 },
 ];
 
 const matches = [
@@ -12,6 +14,7 @@ const matches = [
   }
 
 ];
+
 
 
 
