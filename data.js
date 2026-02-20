f86b497f39dc7aa937369fa42a5546eac0af9602
Syaberu <syaberu9@gmail.com>
2026-02-20 22:09:39 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,20 @@
 const players = [
   { name: "MCsyaberu", tier: "HT4", rating: 0 },
   { name: "AntiUnlockJP", tier: "HT6", rating: 0 },
-  { name: "Super Hiko14", tier: "LT7", rating: 0 },
+  { name: "Super Hiko14", tier: "LT7", rating: 25 },
   { name: "KenNova758", tier: "HT8", rating: 0 },
 ];
 
 const matches = [
   {
     date: "2026-02-18",
-    winner: "test1",
-    loser: "test2",
+    winner: "Super Hiko14",
+    loser: "MCsyaberu",
     change: "+25"
   }
 
 ];
+
 
 
 
