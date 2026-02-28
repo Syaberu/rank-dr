daaaddff67c7ed838fb541bc5090689a6e49b996
Syaberu <syaberu9@gmail.com>
2026-03-01 00:44:24 +0900

    Update data.js

--- data.js
+++ data.js
@@ -67,7 +67,7 @@ const players = [
     tier: "-",
     mapTiers: {
       Cave: { tier: "HT5", record: 49.464 },
-      Flora: { tier: "-", record: 120.000 },
+      Flora: { tier: "HT4", record: 66.164 },
       Temple: { tier: "-", record: 999.999 },
       Factory: { tier: "-", record: 999.999 }
     }
@@ -169,6 +169,7 @@ const TIER_ORDER = [
   "HT1", "LT1", "HT2", "LT2", "HT3", "LT3", "HT4", "LT4", "HT5", "LT5",
   "HT6", "LT6", "HT7", "LT7", "HT8", "LT8", "HT9", "LT9", "HT10", "LT10", "-"
 ];
+
 
 
 
