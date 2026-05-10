e7e210bc33d9532bd0b0aef137bf9d15e69d3a40
Syaberu <syaberu9@gmail.com>
2026-05-10 20:19:12 +0900

    Update data.js

--- data.js
+++ data.js
@@ -34,7 +34,7 @@ const players = [
       Flora: { tier: "HT3", record: 64.941 },
       Gardens: { tier: "LT3", record: 63.579 },
       Metropolis: { tier: "HT4", record: 43.991 },
-      Primus: { tier: "-", record: 63.037 }
+      Primus: { tier: "HT5", record: 63.037 }
     }
   },
   {
